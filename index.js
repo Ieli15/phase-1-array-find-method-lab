accaa2ba17c2723f8b29adb6f6d3e0bc9ec43c0d
Elias Bosire Ayunga <bosire.elias@gmail.com>
2024-12-06 07:41:00 +0300

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "W"},
+    { year: "2012", result: "W"},
+    { year: "2011", result: "N/A"},
+    { year: "2010", result: "L"},
+    { year: "2009", result: "W"},
+
+
+  ]
+  function superbowlWin(record) {
+    const win = record.find(game => game.result === "W");    
+        return win ? win.year : undefined;
+  }
+  
+  console.log(superbowlWin(record));
